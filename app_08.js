fa4f4ae7982cf40b2cbb7669accfe293896b8949
Jari Mutanen <jari.mutanen@genexis.eu>
2020-01-17 15:39:39 +0200

    added style sheet and remove button app_10.css

---
+++ app_08.js
@@ -0,0 +1,116 @@
+//tehtävä 7 lisätään nappi jolla voidaan poistaa 
+
+/******************************************************
+Jatkotehtävä:
+MemoApp app_07.js
+- Lisää Delete-nappi, jolla voi poistaa noten. (input type=number name="index")
+-Huom, jokaisen noten perään delete painike jolla ko. note poistetaan
+-input type='hidden' value="${index}"
+******************************************************/
+
+
+//nodejs on sisäänrakennettuja moduuleja. Alla esimerkki miten kutsutaan http-modulia
+
+const http = require('http');
+const fs = require('fs');
+const notes = [];
+
+const server=http.createServer((req, res)=>{
+        const url=req.url;
+        const method=req.method;
+        console.log(`HTTP request received: url=${url}, method=${method}`);
+
+        if(url === '/') {
+        //res.write('Hello');
+        res.write(`
+        <html>
+        <head><title>MemoAppi</title></head>
+        <body>`);
+                notes.forEach((value, index) => {
+                        res.write(`
+                        <div>${value}, index: ${index}, 
+                        <form action="del-note" method="POST">
+                        <input type="hidden" name="index", value="${index}">
+                        <button type="submit">Delete</button>
+                        </form>
+                        </div>`);
+                });
+
+        res.write(`<form action="add-note" method="POST">
+                <input type="text" name="note">
+                <button type="submit">Add note</button>
+                </form>
+        </body>
+        </html>
+        `);
+        res.statusCode = 200 //OK
+        res.end();
+        return;
+        }
+
+        else if(url === '/add-note') {
+                console.log('/add-note');
+                const chunks = [];
+                req.on('data', (chunk) => {
+                        chunks.push(chunk);
+                });
+
+        
+                req.on('end', () => {
+                   const body = Buffer.concat(chunks).toString();  
+                   const note = body.split('=')[1];
+                   notes.push(note);
+                   //console.log(body);  
+                   res.statusCode = 303; //Redirect
+                   res.setHeader('Location', '/');
+                   res.end();
+                });
+        return;
+        }
+
+        else if(url === '/del-note') {
+                console.log('/del-note');
+                const chunks = [];
+                req.on('data', (chunk) => {
+                        chunks.push(chunk);
+                });
+
+        
+                req.on('end', () => {
+                   const body = Buffer.concat(chunks).toString();  
+                   const index = body.split('=')[1];
+                   notes.splice(index, 1);
+                   //console.log(body);  
+                   res.statusCode = 303; //Redirect
+                   res.setHeader('Location', '/');
+                   res.end();
+                });
+        return;
+        }
+
+
+        else if (url === '/favicon.ico') {
+                fs.readFile('./favicon.ico', (err, data) => {
+                        res.write(data);
+                        res.end();
+                });
+                return;
+        }
+
+        console.log(`${url} not found`);
+        res.write(`
+        <html>
+        <head><title>MemoApp - 404</title></head>
+        <body>
+        <h1>404 Page not found!</h1>
+        </body>
+        </html> `);
+        res.statusCode = 404 //Virhe not found
+        res.end();
+        
+        
+       
+
+});
+
+server.listen(8080);
---
+++ app_09.js
@@ -0,0 +1,117 @@
+//tehtävä 7 lisätään nappi jolla voidaan poistaa 
+
+/******************************************************
+Jatkotehtävä:
+Lisää UTF-8 koodaus ääkkösiä varten
+******************************************************/
+
+
+//nodejs on sisäänrakennettuja moduuleja. Alla esimerkki miten kutsutaan http-modulia
+
+const http = require('http');
+const fs = require('fs');
+const notes = [];
+
+const server=http.createServer((req, res)=>{
+        const url=req.url;
+        const method=req.method;
+        console.log(`HTTP request received: url=${url}, method=${method}`);
+
+        if(url === '/') {
+        //res.write('Hello');
+        res.write(`
+        <html>
+        <head><title>MemoAppi</title>
+        <meta http-equiv="Content-Type", content="text/html;charset=UTF-8">
+        </head>
+        <body>`);
+
+                notes.forEach((value, index) => {
+                        res.write(`
+                        <div>${value}, index: ${index}, 
+                        <form action="del-note" method="POST">
+                        <input type="hidden" name="index", value="${index}">
+                        <button type="submit">Delete</button>
+                        </form>
+                        </div>`);
+                });
+
+        res.write(`<form action="add-note" method="POST">
+                <input type="text" name="note">
+                <button type="submit">Add note</button>
+                </form>
+        </body>
+        </html>
+        `);
+        res.statusCode = 200 //OK
+        res.end();
+        return;
+        }
+
+        else if(url === '/add-note') {
+                console.log('/add-note');
+                const chunks = [];
+                req.on('data', (chunk) => {
+                        chunks.push(chunk);
+                });
+
+        
+                req.on('end', () => {
+                   const body = Buffer.concat(chunks).toString();  
+                   const decoded_body = decodeURIComponent(body);
+                   const note = decoded_body.split('=')[1];
+                   notes.push(note);
+                   //console.log(body);  
+                   res.statusCode = 303; //Redirect
+                   res.setHeader('Location', '/');
+                   res.end();
+                });
+        return;
+        }
+
+        else if(url === '/del-note') {
+                console.log('/del-note');
+                const chunks = [];
+                req.on('data', (chunk) => {
+                        chunks.push(chunk);
+                });
+
+        
+                req.on('end', () => {
+                   const body = Buffer.concat(chunks).toString();  
+                   const index = body.split('=')[1];
+                   notes.splice(index, 1);
+                   //console.log(body);  
+                   res.statusCode = 303; //Redirect
+                   res.setHeader('Location', '/');
+                   res.end();
+                });
+        return;
+        }
+
+
+        else if (url === '/favicon.ico') {
+                fs.readFile('./favicon.ico', (err, data) => {
+                        res.write(data);
+                        res.end();
+                });
+                return;
+        }
+
+        console.log(`${url} not found`);
+        res.write(`
+        <html>
+        <head><title>MemoApp - 404</title></head>
+        <body>
+        <h1>404 Page not found!</h1>
+        </body>
+        </html> `);
+        res.statusCode = 404 //Virhe not found
+        res.end();
+        
+        
+       
+
+});
+
+server.listen(8080);
